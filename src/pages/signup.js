c7c793762be422d00de7424bfd75fa873a18da2c
christianraymond <ombenisn.raymond@gmail.com>
2020-10-07 20:47:40 +0200

    documented ReadMe file

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -9,8 +9,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import axios from "axios";
-
 //Redux staff
 import { connect } from 'react-redux';
 import { signupUser } from '../redux/actions/userAtion'
